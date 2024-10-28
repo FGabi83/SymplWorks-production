053a59e6883498b4d338b7ccd5a61f2b8123ee34
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-10-28 15:45:56 +0100

    hero redesign and slight changes

--- javascripts/modules/dropdownMenu.js
+++ javascripts/modules/dropdownMenu.js
@@ -1,13 +1,17 @@
 function dropdownMenu() {
   const navMenu = document.querySelector(".js-nav--menu");
   const navWrapper = document.querySelector(".js-nav--wrapper");
-  
+  const navLinks = document.querySelectorAll(".js-nav--item");
   
   navMenu.addEventListener("click", () => { 
     navWrapper.classList.toggle("js-is-open");
   }); 
 
-  
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navWrapper.classList.remove("js-is-open");
+    });
+  });
 }
 
 export default dropdownMenu;
